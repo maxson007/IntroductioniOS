a3637acf3e50da37c3cfda92ab90c326f0aaf756
Aymane <danielayman@gmail.com>
2020-10-10 18:02:37 +0200

    Initial Commit

---
+++ Teki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teki
+//
+//  Created by Aymane DANIEL on 10/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
