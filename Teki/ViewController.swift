9c69a1105cd2f0df4b7b59cf4b83a894a37bafc9
Aymane <danielayman@gmail.com>
2020-11-07 13:25:15 +0100

    save

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson"]
+    let activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeQuote() {
+        // On séléctionne un élément alétoire parmi les célébrités
+        let randomIndex1 = Int(arc4random_uniform(UInt32(celebrities.count)))
+        let celebrity = celebrities[randomIndex1]
+        
+        // On séléctionne un élément aléatoire parmi les activités
+        let randomIndex2 = Int(arc4random_uniform(UInt32(activities.count)))
+        let activity = activities[randomIndex2]
+        
+        // On construit la citation et on l'affecte au texte du label
+        let quote = "Tu es " + celebrity + " " + activity + " !"
+        quoteLabel.text = quote
+    }
+    
 }
 
